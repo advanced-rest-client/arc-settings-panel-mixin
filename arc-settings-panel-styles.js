29f5a6beb3ffc8aaf2b50b2e5e3b2a16cbc8a097
Pawel Psztyc <jarrodek@gmail.com>
2019-04-06 08:16:48 -0700

    Update: Updating import paths to match web spec

--- arc-settings-panel-styles.js
+++ arc-settings-panel-styles.js
@@ -11,7 +11,7 @@ WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 License for the specific language governing permissions and limitations under
 the License.
 */
-import '@polymer/polymer/polymer-element.js';
+import '../../@polymer/polymer/polymer-element.js';
 const styleElement = document.createElement('dom-module');
 styleElement.innerHTML =
 `<template>
